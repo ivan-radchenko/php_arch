702720e4d9ec323ef25c83f4f9bdf3792affb6ca
Ivan <92185577+ivan-radchenko@users.noreply.github.com>
2023-04-25 20:55:44 +0300

    homework9_done

---
+++ sort/HeapSplSort.php
@@ -0,0 +1,16 @@
+<?php
+
+function treeSort(array $list): array
+{
+    $tree = new SplMinHeap();
+    foreach ($list as $n) {
+        $tree->insert($n);
+    }
+    $list = [];
+    while ($tree->valid()) {
+        $list[] = $tree->top();
+        $tree->next();
+    }
+    return $list;
+}
+
